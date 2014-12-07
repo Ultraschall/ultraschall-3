87dd1e404039079a8667427877ae575fa8e13a49
Heiko Panjas <heiko@panjas.com>
2014-12-07 23:07:48 +0100

    Added driver installer

--- make-ultraschall-package.sh
+++ make-ultraschall-package.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-productbuild --distribution ./Scripts/distribution.xml --resources ./Resources --package-path ./Build --package-path ./Modules/Driver ./Payload/UltraschallHub-1.2.pkg
+productbuild --distribution ./Scripts/distribution.xml --resources ./Resources --package-path ./Build ./Payload/UltraschallHub-1.2.pkg
+
+productbuild --distribution ./Scripts/distribution_driver.xml --resources ./Resources --package-path ./Modules/Driver ./Payload/UltraschallDriver-1.2.pkg
