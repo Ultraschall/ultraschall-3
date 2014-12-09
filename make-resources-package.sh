1aed30b12a97d1f40292ed20f3e855a396c3c2df
Heiko Panjas <heiko@panjas.com>
2014-12-09 23:36:17 +0100

    Installation of splash screen

---
+++ make-resources-package.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Create Ultraschall Resources package
+pkgbuild --root ./Modules/Resources/Payload --identifier fm.ultraschall.Resources --install-location /Library ./Build/UltraschallResources-1.2.pkg
+
+
