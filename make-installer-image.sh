3ae3e83217491a0736aa60f77421b359de0afb12
Heiko Panjas <heiko@panjas.com>
2014-12-22 14:07:05 +0100

    Release Candidate 5

--- make-installer-image.sh
+++ make-installer-image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if [ -f UltraschallHub-1.2_rc4.dmg ]; then
-	rm -f UltraschallHub-1.2_rc4.dmg
+if [ -f UltraschallHub-1.2_rc5.dmg.gz ]; then
+	rm -f UltraschallHub-1.2_rc5.dmg.gz
 fi
 
 make-plugin-package.sh
@@ -10,5 +10,6 @@ make-resources-package.sh
 make-soundfloweruninstaller-package.sh
 make-ultraschall-package.sh
 
-hdiutil create -size 50m -srcfolder ./Payload -fs HFS+ -volname UltraschallHub-1.2_RELEASE_CANDIDATE_4 ./UltraschallHub-1.2_rc4.dmg
+hdiutil create -size 50m -srcfolder ./Payload -fs HFS+ -volname UltraschallHub-1.2_RELEASE_CANDIDATE_5 ./UltraschallHub-1.2_rc5.dmg
+gzip ./UltraschallHub-1.2_rc5.dmg
 
