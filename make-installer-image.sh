e2ff6179f56a7d9583e45a5aba715ddd10da790d
Heiko Panjas <heiko@panjas.com>
2015-05-22 00:49:55 +0200

    1.3beta3

--- make-installer-image.sh
+++ make-installer-image.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if [ -f Ultraschall-1.3beta1.dmg ]; then
-	rm -f Ultraschall-1.3beta1.dmg
+if [ -f Ultraschall-1.3beta2.dmg ]; then
+	rm -f Ultraschall-1.3beta2.dmg
 fi
 
-if [ -f Ultraschall-1.3beta1.dmg.zip ]; then
-	rm -f Ultraschall-1.3beta1.dmg.zip
+if [ -f Ultraschall-1.3beta2.dmg.zip ]; then
+	rm -f Ultraschall-1.3beta2.dmg.zip
 fi
 
 make-plugin-package.sh
@@ -14,11 +14,11 @@ make-resources-package.sh
 make-soundfloweruninstaller-package.sh
 make-ultraschall-package.sh
 
-productsign --sign "Developer ID Installer: Heiko Panjas (8J2G689FCZ)" Payload/Ultraschall-1.3beta1-unsigned.pkg Payload/Ultraschall-1.3beta1.pkg
-rm Payload/Ultraschall-1.3beta1-unsigned.pkg
+productsign --sign "Developer ID Installer: Heiko Panjas (8J2G689FCZ)" Payload/Ultraschall-unsigned.pkg Payload/Ultraschall-1.3beta2.pkg
+rm Payload/Ultraschall-unsigned.pkg
 
-hdiutil create -size 50m -srcfolder ./Payload -fs HFS+ -volname "Ultraschall 1.3beta1" ./Ultraschall-1.3beta1.dmg
-zip Ultraschall-1.3beta1.dmg.zip Ultraschall-1.3beta1.dmg
-rm Ultraschall-1.3beta1.dmg
+hdiutil create -size 100m -srcfolder ./Payload -fs HFS+ -volname "Ultraschall 1.3beta2" ./Ultraschall-1.3beta2.dmg
+zip Ultraschall-1.3beta2.dmg.zip Ultraschall-1.3beta2.dmg
+rm Ultraschall-1.3beta2.dmg
 
 
--- make-resources-package.sh
+++ make-resources-package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Create Ultraschall Resources package
-pkgbuild --root ./Modules/Resources/Payload --identifier fm.ultraschall.Resources --install-location /Library ./Build/UltraschallResources-1.3beta1.pkg
+pkgbuild --root ./Modules/Resources/Payload --identifier fm.ultraschall.Resources --install-location /Library ./Build/UltraschallResources.pkg
 
 
