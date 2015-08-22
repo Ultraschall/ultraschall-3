d42a08ddc0b1219ca7e8016ba6d5311560d5d5b9
Heiko Panjas <heiko@panjas.com>
2015-08-22 11:02:28 +0200

    Release 1.3.2

--- make-installer-image.sh
+++ make-installer-image.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if [ -f Ultraschall-1.3.1.dmg ]; then
-	rm -f Ultraschall-1.3.1.dmg
+if [ -f Ultraschall-1.3.2.dmg ]; then
+	rm -f Ultraschall-1.3.2.dmg
 fi
 
-if [ -f Ultraschall-1.3.1.dmg.zip ]; then
-	rm -f Ultraschall-1.3.1.dmg.zip
+if [ -f Ultraschall-1.3.2.dmg.zip ]; then
+	rm -f Ultraschall-1.3.2.dmg.zip
 fi
 
 make-plugin-package.sh
@@ -14,11 +14,11 @@ make-resources-package.sh
 make-soundfloweruninstaller-package.sh
 make-ultraschall-package.sh
 
-productsign --sign "Developer ID Installer: Heiko Panjas (8J2G689FCZ)" Payload/Ultraschall-unsigned.pkg Payload/Ultraschall-1.3.1.pkg
+productsign --sign "Developer ID Installer: Heiko Panjas (8J2G689FCZ)" Payload/Ultraschall-unsigned.pkg Payload/Ultraschall-1.3.2.pkg
 rm Payload/Ultraschall-unsigned.pkg
 
-hdiutil create -size 100m -srcfolder ./Payload -fs HFS+ -volname "Ultraschall 1.3.1" ./Ultraschall-1.3.1.dmg
-zip Ultraschall-1.3.1.dmg.zip Ultraschall-1.3.1.dmg
-rm Ultraschall-1.3.1.dmg
+hdiutil create -size 100m -srcfolder ./Payload -fs HFS+ -volname "Ultraschall 1.3.2" ./Ultraschall-1.3.2.dmg
+zip Ultraschall-1.3.2.dmg.zip Ultraschall-1.3.2.dmg
+rm Ultraschall-1.3.2.dmg
 
 
