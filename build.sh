022758d20bed6cb5006b6920a1afdcb8544afb38
Heiko Panjas <heiko@panjas.com>
2016-02-04 22:31:45 +0100

    updated for version 2.1

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export ULTRASCHALL_RELEASE=Ultraschall-2.0
+export ULTRASCHALL_RELEASE=Ultraschall-2.1
 export ULTRASCHALL_RELEASE_DISK1=$ULTRASCHALL_RELEASE.dmg
 
 # Cleanup old installer image
@@ -61,10 +61,10 @@ pkgbuild --root ../Soundboard/Files/VST --identifier fm.ultraschall.Soundboard.V
 pkgbuild --root ../Soundboard/Extras --identifier fm.ultraschall.Soundboard.Extras --install-location /Library/Application\ Support/Ultraschall ./Build/UltraschallSoundboardExtras.pkg
 
 # Create SWS REAPER Plugin Extension package
-pkgbuild --root ./3rdParty/SWS/UserPlugins/Payload --scripts ./3rdParty/SWS/UserPlugins/Scripts --identifier com.mj-s.sws --install-location /Library/Application\ Support/REAPER/UserPlugins ./Build/SWS_Extension-2.8.1.pkg
+pkgbuild --root ./3rdParty/SWS/UserPlugins/Payload --scripts ./3rdParty/SWS/UserPlugins/Scripts --identifier com.mj-s.sws --install-location /Library/Application\ Support/REAPER/UserPlugins ./Build/SWS_Extension-2.8.3.pkg
 
 # Create SWS REAPER Plugin Scripts package
-pkgbuild --root ./3rdParty/SWS/Scripts/Payload --scripts ./3rdParty/SWS/Scripts/Scripts --identifier com.mj-s.sws.Scripts --install-location /Library/Application\ Support/REAPER/Scripts ./Build/SWS_ExtensionScripts-2.8.1.pkg
+pkgbuild --root ./3rdParty/SWS/Scripts/Payload --scripts ./3rdParty/SWS/Scripts/Scripts --identifier com.mj-s.sws.Scripts --install-location /Library/Application\ Support/REAPER/Scripts ./Build/SWS_ExtensionScripts-2.8.3.pkg
 
 # Create Ultraschall Resources package
 pkgbuild --root ../REAPER/Plugin/Resources --identifier fm.ultraschall.Resources --install-location /Library/Application\ Support/Ultraschall ./Build/UltraschallResources.pkg
@@ -80,7 +80,7 @@ productbuild --distribution ./Scripts/distribution.xml --resources ./Resources -
 rm -rf ./Build
 
 # Create final signed installer package
-productsign --sign "Developer ID Installer: Heiko Panjas (8J2G689FCZ)" ./Payload/Ultraschall-unsigned.pkg ./Payload/Ultraschall-2.0.pkg
+productsign --sign "Developer ID Installer: Heiko Panjas (8J2G689FCZ)" ./Payload/Ultraschall-unsigned.pkg ./Payload/Ultraschall-2.1.pkg
 rm -f ./Payload/Ultraschall-unsigned.pkg
 
 # Create signature on uninstall script
