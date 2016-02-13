89d0f342ec5ad2729b1166aa14e21af1c95de92e
Heiko Panjas <heiko@panjas.com>
2016-02-13 23:20:48 +0100

    updated for ultraschall version 2.1

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export ULTRASCHALL_RELEASE=Ultraschall-2.1_prerelase_1
+export ULTRASCHALL_RELEASE=Ultraschall-2.1_prerelase_2
 export ULTRASCHALL_RELEASE_DISK1=$ULTRASCHALL_RELEASE.dmg
 
 # Cleanup old installer image
