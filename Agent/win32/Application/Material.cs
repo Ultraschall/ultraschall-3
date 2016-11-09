c21d7512e0b4edc65fcc6a28ca25ff655b992bbf
Heiko Panjas <heiko@panjas.com>
2016-11-09 19:41:09 +0100

    initial revision

---
+++ Agent/win32/Application/Material.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ultraschall {
+   public class Material {
+      public Material(String component, String version, String location) {
+         this.component_ = component;
+         this.version_ = version;
+         this.location_ = location;
+      }
+
+      private String component_ = null;
+      public String Component {
+         get {
+            return component_;
+         }
+      }
+
+      private String version_ = null;
+
+      public String Version {
+         get {
+            return this.version_;
+         }
+      }
+
+      private String location_ = null;
+
+      public String Location {
+         get {
+            return this.location_;
+         }
+      }
+   }
+}
