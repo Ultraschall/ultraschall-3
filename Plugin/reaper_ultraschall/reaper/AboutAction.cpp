1e94758fda4f3fa582fc811facce294d2cbf50e7
Heiko Panjas <heiko@panjas.com>
2017-02-25 22:31:28 +0100

    update for version 3.0 beta6

--- Plugin/reaper_ultraschall/reaper/AboutAction.cpp
+++ Plugin/reaper_ultraschall/reaper/AboutAction.cpp
@@ -97,7 +97,7 @@ REAPER ";
    message2 += QueryRawReaperVersion();
    message2 += "\r\n";
 
-   NotificationWindow::Show("About Ultraschall 3.0 \"Miedinger\" beta5...", message1 + message2);
+   NotificationWindow::Show("About Ultraschall 3.0 \"Miedinger\" beta6...", message1 + message2);
 
    return SERVICE_SUCCESS;
 }
