17184f65500286f4891e8c5f6f06a367ae36350f
Ralf Stockmann <rstockm@gwdg.de>
2017-02-26 22:04:31 +0100

    Better handling for comma

--- Plugin/Scripts/Shared/ultraschall_edit_id3_tags.lua
+++ Plugin/Scripts/Shared/ultraschall_edit_id3_tags.lua
@@ -31,37 +31,83 @@ function Msg(val)
 end
 
 -- Used to escape "'s by toCSV
-function escapeCSV (s)
-  if string.find(s, '[,"]') then
-    s = '"' .. string.gsub(s, '"', '""') .. '"'
-  end
-  return s
-end
+--function escapeCSV (s)
+--  _, count = string.gsub(s, "\"", "\"")
+--  if count==nil then count=0 end
+--  if math.fmod(count,2)==1 then --if we have a odd number of " replace them with "" 
+--    if string.find(s, '["]') then
+--      s = '"' .. string.gsub(s, '"', '""') .. '"'
+--    end
+--  elseif string.find(s,",")~=nil then
+--    s="\""..s.."\""
+--  end
+--  return s
+--end
 
 function notes2csv ()
   local csv = ""
+  linenumber=1
   notes = reaper.GetSetProjectNotes(0, false, "")
     for line in notes:gmatch"[^\n]*" do
-      csv = csv .. "," .. escapeCSV(line)
-    end  
-  return string.sub(csv, 2) -- remove first ","
+      csv = csv .. "," .. line --escapeCSV(line)
+    end
+    
+    retval= string.sub(csv, 2) -- remove first ","
+  return retval
 end
 
-function csv2lines (csv)
-  csv = string.gsub(csv, ", ", ";") --safe the regular commas. Yes, awkward.
-  csv = string.gsub(csv, ",", "\n") -- replace comma with linebreak
-  new_notes = string.gsub(csv, ";", ", ") -- back to comma
-  -- Msg(new_notes)
-  return new_notes
+function csv2lines (line)
+  pos=0
+  pos_old=1
+  clean=""
+  for i=1, 5,1 do
+    pos=string.find(result,",",pos_old)
+    substring=string.sub(line,pos_old,pos-1)
+    clean=clean..substring.."\n"
+    pos_old=pos+1
+  end
+  --check field 6
+  substring=string.sub(line,pos_old)
+  clean=clean..substring
+  return clean
 end
 
-
+--reaper.ShowConsoleMsg("") --clear console
 dialog_ret_vals = notes2csv() --default values
-retval, result = reaper.GetUserInputs("Edit ID3 Podcast Metadata", 6, "Title:,Artist:,Album (Podcast):,Year:,Genre:,Comment:", dialog_ret_vals)
+
+retval, result = reaper.GetUserInputs("Edit ID3 Podcast Metadata", 6, "Title (no comma allowed):,Artist (no comma allowed):,Album (Podcast, no comma allowed):,Year (no comma allowed):,Genre (no comma allowed):,Comment (you may use comma):", dialog_ret_vals)
 if retval == true then
-  -- Msg(result)
+  --step through field 1-5 and check if the numer of " is even. Add a " to the end if needed.
+  pos=0
+  pos_old=1
+  clean=""
+  for i=1, 5,1 do
+    pos=string.find(result,",",pos_old)
+    substring=string.sub(result,pos_old,pos-1)
+    _, count = string.gsub(substring, "\"", "\"")
+    if math.fmod(count,2)==1 then substring=substring.."\"" end
+    clean=clean..substring..","
+    pos_old=pos+1
+  end
+  --check field 6
+  substring=string.sub(result,pos_old)
+  _, count = string.gsub(substring, "\"", "\"")
+  if math.fmod(count,2)==1 then substring=substring.."\"" end
+  clean=clean..substring
+  result=clean
+  
+  -- if more than  "," 5 then escape the comment field (surround with ")
+  _, count = string.gsub(result, ",", ",")
+  if count>5 then 
+    pos=0
+    for i=1, 5,1 do
+      pos=string.find(result,",",pos+1)
+    end
+    newresult=string.sub(result,1,pos).."\""..string.sub(result,pos+1).."\""
+    result=newresult
+  end
+
+
+
   notes = reaper.GetSetProjectNotes(0, true, csv2lines(result)) -- write new notes
 end
-
-
-
