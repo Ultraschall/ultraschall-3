6258e0847c88f29047ea13d5c10b5d178b34b7fa
Ralf Stockmann <rstockm@gwdg.de>
2017-02-27 17:20:16 +0100

    Handling for state = recording added

--- Plugin/Scripts/Shared/ultraschall_onair.lua
+++ Plugin/Scripts/Shared/ultraschall_onair.lua
@@ -29,31 +29,63 @@ function Msg(val)
 	reaper.ShowConsoleMsg(tostring(val).."\n")
 end
 
-
-reaper.Undo_BeginBlock()
-
-m = reaper.GetMasterTrack(0)                                                  --streaming is always on the master track
--- fx_name_retval, fx_name = reaper.TrackFX_GetFXName(m, 0, "")           --get the name of the first effect, debug only
-
-os = reaper.GetOS()
-if string.match(os, "OSX") then 
-	fx_slot = reaper.TrackFX_GetByName(m, "ITSR: StudioLinkOnAir", 1)      --get the slot of the StudioLink effect. If there is none: initiate one.
-else	-- Windows
-	fx_slot = reaper.TrackFX_GetByName(m, "StudioLinkOnAir (IT-Service Sebastian Reimers)", 1)      --get the slot of the StudioLink effect. If there is none: initiate one.
-end
-
-
 is_new,name,sec,cmd,rel,res,val = reaper.get_action_context()
-state = reaper.GetToggleCommandStateEx(sec, cmd)                           --get state of the OnAir Button: on/off
+state = reaper.GetToggleCommandStateEx(sec, cmd)                           --get state of the OnAir Button: on (1) /off (0)
 
-if state == 0 then                                                                  --streaming is off: start streaming
-	reaper.SetToggleCommandState(sec, cmd, 1)
-	test2 = reaper.TrackFX_SetEnabled(m, fx_slot, true)
-else                                                                                --streaming is on: stop streaming
-	reaper.SetToggleCommandState(sec, cmd, 0)
-	reaper.SNM_MoveOrRemoveTrackFX(m, fx_slot, 0)							-- remove FX
-end     
 
-reaper.RefreshToolbar2(sec, cmd)
+playstate = reaper.GetPlayState()
+-- reaper.ShowConsoleMsg(state)
+if playstate == 5 and state == 1 then -- is recording and stream active
 
-reaper.Undo_EndBlock("Ultraschall toggle StudioLink OnAir", -1)
+	--[[type:
+	0=OK,
+	1=OKCANCEL,
+	2=ABORTRETRYIGNORE,
+ 	3=YESNOCANCEL,
+ 	4=YESNO,
+ 	5=RETRYCANCEL]]
+
+	type = 0
+	title = "Active Recording"
+	msg = "You have to stop the recording before you can stop broadcasting."
+	result = reaper.ShowMessageBox( msg, title, type )
+
+	--[[result:
+	1=OK,
+ 	2=CANCEL,
+ 	3=ABORT,
+ 	4=RETRY,
+ 	5=IGNORE,
+ 	6=YES,
+ 	7=NO
+	]]
+
+	if result == 1 then -- abort
+		return
+	end
+else
+	reaper.Undo_BeginBlock()
+
+	m = reaper.GetMasterTrack(0)                                                  --streaming is always on the master track
+	-- fx_name_retval, fx_name = reaper.TrackFX_GetFXName(m, 0, "")           --get the name of the first effect, debug only
+
+	os = reaper.GetOS()
+	if string.match(os, "OSX") then 
+		fx_slot = reaper.TrackFX_GetByName(m, "ITSR: StudioLinkOnAir", 1)      --get the slot of the StudioLink effect. If there is none: initiate one.
+	else	-- Windows
+		fx_slot = reaper.TrackFX_GetByName(m, "StudioLinkOnAir (IT-Service Sebastian Reimers)", 1)      --get the slot of the StudioLink effect. If there is none: initiate one.
+	end
+
+
+	if state == 0 then                                                                  --streaming is off: start streaming
+		reaper.SetToggleCommandState(sec, cmd, 1)
+		test2 = reaper.TrackFX_SetEnabled(m, fx_slot, true)
+	else                                                                                --streaming is on: stop streaming
+		reaper.SetToggleCommandState(sec, cmd, 0)
+		reaper.SNM_MoveOrRemoveTrackFX(m, fx_slot, 0)							-- remove FX
+	end     
+
+	reaper.RefreshToolbar2(sec, cmd)
+
+	reaper.Undo_EndBlock("Ultraschall toggle StudioLink OnAir", -1)
+end
