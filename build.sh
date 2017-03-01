691fedc05cc60f4fb4e29dd04cbbc17b9675822a
Heiko Panjas <heiko@panjas.com>
2017-03-02 00:28:55 +0100

    update background image because of the old pathbar

--- build.sh
+++ build.sh
@@ -122,7 +122,7 @@ echo '
            set current view of container window to icon view
            set toolbar visible of container window to false
            set statusbar visible of container window to false
-           set the bounds of container window to {100, 100, 800, 509}
+           set the bounds of container window to {100, 100, 800, 540}
            set viewOptions to the icon view options of container window
            set arrangement of viewOptions to not arranged
            set background picture of viewOptions to file ".background:background.png"
