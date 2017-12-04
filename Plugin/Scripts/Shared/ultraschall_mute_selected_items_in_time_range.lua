5377c1f9d857e2043c059665024f08e4ce1d1956
Ralf Stockmann <rstockm@gwdg.de>
2017-12-04 04:37:26 +0100

    Unselect all after mute/unmute selection

--- Plugin/Scripts/Shared/ultraschall_mute_selected_items_in_time_range.lua
+++ Plugin/Scripts/Shared/ultraschall_mute_selected_items_in_time_range.lua
@@ -1,3 +1,34 @@
+--[[
+################################################################################
+# 
+# Copyright (c) 2014-2017 Ultraschall (http://ultraschall.fm)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# 
+################################################################################
+]]
+ 
+
+local info = debug.getinfo(1,'S');
+script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
+dofile(script_path .. "ultraschall_helper_functions.lua")
+
 -- Ultraschall mute time selection
 --
 -- add envelope points to mute envelope in time range
@@ -141,6 +172,7 @@ if time_selection_end-time_selection_start>0 then
     muteselectedTracks()
     if number_of_selected_tracks==0 then reaper.Main_OnCommand(40297,0) end --deselect all tracks
   end
+  runcommand("_Ultraschall_Unselect_All")
 end
 
 reaper.Undo_EndBlock("Ultraschall: mute (selected) Items in all (selected) tracks in time selection", 0)
