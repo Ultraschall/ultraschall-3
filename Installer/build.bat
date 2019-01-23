04daa29031661af6419d1c7f787d7c0a0e811d4d
Heiko Panjas <heiko@panjas.com>
2019-01-23 15:07:23 +0100

    enable incremental build

--- Installer/build.bat
+++ Installer/build.bat
@@ -12,28 +12,36 @@ set ULTRASCHALL_RELEASE_LABEL=ULTRASCHALL-3.2-preview-3
 rem Remove previously created installer package
 if exist %ULTRASCHALL_RELEASE_LABEL%.msi del /f /q %ULTRASCHALL_RELEASE_LABEL%.msi
 
-rem Clean-up _payload folder in case the last build went wrong
-rmdir /s /q _payload 2>nul
-if not errorlevel 0 goto failed
+rem Specify build directory
+set ULTRASCHALL_BUILD_DIRECTORY=_build
+
 rem Create folder for intermediate data
-mkdir _payload
-rem Enter _payload folder
-pushd _payload
+if not exist %ULTRASCHALL_BUILD_DIRECTORY% (
+    mkdir %ULTRASCHALL_BUILD_DIRECTORY%
+)
+rem Enter %ULTRASCHALL_BUILD_DIRECTORY% folder
+pushd %ULTRASCHALL_BUILD_DIRECTORY%
 
 echo Copying Microsoft Visual C++ 2017 CRT...
-md vcredist
+if not exist vcredist (
+    mkdir vcredist
+)
 copy "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\VC\Redist\MSVC\14.16.27012\MergeModules\Microsoft_VC141_CRT_x64.msm" vcredist > nul
 if not errorlevel 0 goto failed
 echo Done.
 
 echo Copying Ultraschall Theme...
-mkdir theme
+if not exist theme (
+    mkdir theme
+)
 copy ..\..\Themes\Ultraschall_3.1_WIN.ReaperConfigZip theme\Ultraschall_3.1.ReaperConfigZip > nul
 if not errorlevel 0 goto failed
 echo Done.
 
 echo Copying Ultraschall Add-ons...
-mkdir add-ons
+if not exist add-ons (
+    mkdir add-ons
+)
 copy "..\..\Docs\Keymap.pdf" "add-ons\Ultraschall Keyboard Layout.pdf" > nul
 if not errorlevel 0 goto failed
 copy "..\..\Docs\Keymap.pptx" "add-ons\Ultraschall Keyboard Layout.pptx" > nul
@@ -51,7 +59,9 @@ if not errorlevel 0 goto failed
 echo Done.
 
 echo Building Ultraschall documentation files...
-mkdir resources
+if not exist resources (
+    mkdir resources
+)
 pandoc --from=markdown --to=html --standalone --quiet --self-contained --css=..\..\Tools\ultraschall.css --output=resources\README.html ..\..\README.md
 if not errorlevel 0 goto failed
 pandoc --from=markdown --to=html --standalone --quiet --self-contained --css=..\..\Tools\ultraschall.css --output=resources\INSTALL.html ..\..\INSTALL.md
@@ -61,7 +71,9 @@ if not errorlevel 0 goto failed
 echo Done.
 
 echo Building Ultraschall REAPER Plug-in...
-md plug-in
+if not exist plug-in (
+    mkdir plug-in
+)
 pushd plug-in
 cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release ..\..\..\Plugin > nul
 if not errorlevel 0 goto failed
@@ -70,18 +82,18 @@ if not errorlevel 0 goto failed
 popd
 echo Done.
 
-rem Leave _payload folder
+rem Leave %ULTRASCHALL_BUILD_DIRECTORY% folder
 popd
 
 echo Building installer package...
-candle -nologo -arch x64 -out _payload\%ULTRASCHALL_RELEASE_LABEL%.wixobj Scripts\distribution.wxs
+candle -nologo -arch x64 -out %ULTRASCHALL_BUILD_DIRECTORY%\%ULTRASCHALL_RELEASE_LABEL%.wixobj Scripts\distribution.wxs
 if not errorlevel 0 goto failed
-light -nologo -sw1076 -ext WixUIExtension -cultures:en-us -spdb _payload\%ULTRASCHALL_RELEASE_LABEL%.wixobj -out %ULTRASCHALL_RELEASE_LABEL%.msi
+light -nologo -sw1076 -ext WixUIExtension -cultures:en-us -spdb %ULTRASCHALL_BUILD_DIRECTORY%\%ULTRASCHALL_RELEASE_LABEL%.wixobj -out %ULTRASCHALL_RELEASE_LABEL%.msi
 if not errorlevel 0 goto failed
 echo Done.
 
 rem Clean-up if build completed successfully
-rd /s /q _payload
+set ULTRASCHALL_BUILD_DIRECTORY=
 set ULTRASCHALL_RELEASE_LABEL=
 goto end
 
