836a6d5456d8cb3feaf558f39c52d474b58f6d7a
Heiko Panjas <heiko@panjas.com>
2019-01-26 18:51:13 +0100

    move all picture function into this

---
+++ Plugin/reaper_ultraschall/PictureUtilities.cpp
@@ -0,0 +1,55 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) The Ultraschall Project (http://ultraschall.fm)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "PictureUtilities.h"
+
+namespace ultraschall { namespace reaper {
+
+ImageFormat FindImageFormat(const uint8_t* data, const size_t dataSize)
+{
+    PRECONDITION_RETURN(data != 0, ImageFormat::Unknown);
+    PRECONDITION_RETURN(dataSize > 0, ImageFormat::Unknown);
+
+    ImageFormat format = ImageFormat::Unknown;
+
+    if (dataSize >= 2)
+    {
+        if ((data[0] == 0xff) && (data[1] == 0xd8))
+        {
+            format = ImageFormat::Jpeg;
+        }
+
+        if (dataSize >= 8)
+        {
+            if ((data[0] == 0x89) && (data[1] == 0x50) && (data[2] == 0x4e) && (data[3] == 0x47))
+            {
+                format = ImageFormat::Png;
+            }
+        }
+    }
+
+    return format;
+}
+
+}} // namespace ultraschall::reaper
---
+++ Plugin/reaper_ultraschall/PictureUtilities.h
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) The Ultraschall Project (http://ultraschall.fm)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __ULTRASCHALL_REAPER_IMAGE_TOOLS_H_INCL__
+#define __ULTRASCHALL_REAPER_IMAGE_TOOLS_H_INCL__
+
+#include "Common.h"
+
+namespace ultraschall { namespace reaper {
+
+enum class ImageFormat { Unknown, Jpeg, Png };
+
+ImageFormat FindImageFormat(const uint8_t* data, const size_t dataSize);
+
+}} // namespace ultraschall::reaper
+
+#endif // #ifndef __ULTRASCHALL_REAPER_IMAGE_TOOLS_H_INCL__
