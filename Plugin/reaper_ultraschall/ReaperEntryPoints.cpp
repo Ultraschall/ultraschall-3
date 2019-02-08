eefd83831b72809baa9bc7862c7ad46a92228f3e
Heiko Panjas <heiko@panjas.com>
2019-02-08 23:57:03 +0100

    update license header

--- Plugin/reaper_ultraschall/ReaperEntryPoints.cpp
+++ Plugin/reaper_ultraschall/ReaperEntryPoints.cpp
@@ -2,7 +2,9 @@
 //
 // Copyright (c) The Ultraschall Project (http://ultraschall.fm)
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
+// Permission is hereby granted, free of charge, to any person obtaining a copy// The MIT License (MIT)
+//
+
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
