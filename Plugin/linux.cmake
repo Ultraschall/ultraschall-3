7d2489fadaf4ed1613fa75923edf29ac0cb81fc0
Heiko Panjas <heiko@panjas.com>
2019-04-14 00:22:18 +0200

    build control file for linux

--- Plugin/linux.cmake
+++ Plugin/linux.cmake
@@ -1,2 +1,192 @@
 
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG "$ENV{HOME}/.config/REAPER/UserPlugins")
+
+include(ExternalProject)
+
+# configure zlib
+
+set(CURRENT_EXTERNAL_PROJECT zlib)
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ...")
+
+ExternalProject_Add(zlib
+    PREFIX libz
+	GIT_REPOSITORY https://github.com/madler/zlib.git
+    GIT_TAG v1.2.11
+    STEP_TARGETS build
+    EXCLUDE_FROM_ALL TRUE
+    CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF
+)
+
+ExternalProject_Get_Property(zlib SOURCE_DIR)
+ExternalProject_Get_Property(zlib BINARY_DIR)
+
+set(LIBZ_INCLUDE_PATH
+    ${BINARY_DIR}
+    ${SOURCE_DIR}
+    ${SOURCE_DIR}/contrib/minizip
+)
+
+set(LIBZ_LIBRARY_PATH ${BINARY_DIR}/${CMAKE_BUILD_TYPE}/libz.a)
+set(LIBZ_SOURCE_PATH ${SOURCE_DIR})
+
+message(STATUS "LIBZ_INCLUDE_PATH = ${LIBZ_INCLUDE_PATH}")
+message(STATUS "LIBZ_LIBRARY_PATH = ${LIBZ_LIBRARY_PATH}")
+message(STATUS "LIBZ_SOURCE_PATH  = ${LIBZ_SOURCE_PATH}")
+
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
+
+# configure curl
+
+set(CURRENT_EXTERNAL_PROJECT curl)
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ...")
+
+ExternalProject_Add(curl
+    PREFIX libcurl
+	GIT_REPOSITORY https://github.com/curl/curl.git
+	GIT_TAG curl-7_63_0
+    STEP_TARGETS build
+    EXCLUDE_FROM_ALL TRUE
+	CMAKE_ARGS -DBUILD_CURL_EXE=OFF -DBUILD_SHARED_LIBS=OFF -DHTTP_ONLY=ON -DENABLE_IPV6=ON -DCMAKE_USE_DARWINSSL=ON -DCURL_CA_PATH=none -DBUILD_TESTING=OFF
+)
+
+ExternalProject_Get_Property(curl SOURCE_DIR)
+ExternalProject_Get_Property(curl BINARY_DIR)
+
+set(LIBCURL_INCLUDE_PATH ${SOURCE_DIR}/include)
+
+if(CMAKE_BUILD_TYPE MATCHES Debug)
+    set(LIBCURL_LIBRARY_PATH ${BINARY_DIR}/lib/${CMAKE_BUILD_TYPE}/libcurl-d.a)
+else()
+    set(LIBCURL_LIBRARY_PATH ${BINARY_DIR}/lib/${CMAKE_BUILD_TYPE}/libcurl.a)
+endif()
+
+message(STATUS "LIBCURL_INCLUDE_PATH = ${LIBCURL_INCLUDE_PATH}")
+message(STATUS "LIBCURL_LIBRARY_PATH = ${LIBCURL_LIBRARY_PATH}")
+
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
+
+# configure mp4v2
+
+set(CURRENT_EXTERNAL_PROJECT mp4v2)
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ...")
+
+include(ExternalProject)
+ExternalProject_Add(mp4v2
+    PREFIX libmp4v2
+	GIT_REPOSITORY https://github.com/Ultraschall/mp4v2.git
+	GIT_TAG v2.1.0
+    STEP_TARGETS build
+    EXCLUDE_FROM_ALL TRUE
+	CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF -DMP4V2_EXPORTS=0 -DMP4V2_NO_STDINT_DEFS=1
+)
+
+ExternalProject_Get_Property(mp4v2 SOURCE_DIR)
+ExternalProject_Get_Property(mp4v2 BINARY_DIR)
+
+set(LIBMP4V2_INCLUDE_PATH ${SOURCE_DIR}/include)
+set(LIBMP4V2_LIBRARY_PATH ${BINARY_DIR}/${CMAKE_BUILD_TYPE}/libmp4v2.a)
+
+message(STATUS "LIBMP4V2_INCLUDE_PATH = ${LIBMP4V2_INCLUDE_PATH}")
+message(STATUS "LIBMP4V2_LIBRARY_PATH = ${LIBMP4V2_LIBRARY_PATH}")
+
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
+
+# configure taglib
+
+set(CURRENT_EXTERNAL_PROJECT taglib)
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ...")
+
+include(ExternalProject)
+ExternalProject_Add(taglib
+    PREFIX libtag
+	GIT_REPOSITORY https://github.com/taglib/taglib.git
+	GIT_TAG v1.11.1
+    STEP_TARGETS build
+    EXCLUDE_FROM_ALL TRUE
+	CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_BINDINGS=OFF
+)
+
+ExternalProject_Get_Property(taglib SOURCE_DIR)
+ExternalProject_Get_Property(taglib BINARY_DIR)
+
+set(LIBTAG_INCLUDE_PATH
+    ${BINARY_DIR} # taglib_config.h
+    ${SOURCE_DIR}/taglib
+    ${SOURCE_DIR}/taglib/toolkit
+    ${SOURCE_DIR}/taglib/mpeg
+    ${SOURCE_DIR}/taglib/mpeg/id3v2
+    ${SOURCE_DIR}/taglib/mpeg/id3v2/frames
+)
+
+set(LIBTAG_LIBRARY_PATH   ${BINARY_DIR}/taglib/${CMAKE_BUILD_TYPE}/libtag.a)
+
+message(STATUS "LIBTAG_INCLUDE_PATH = ${LIBTAG_INCLUDE_PATH}")
+message(STATUS "LIBTAG_LIBRARY_PATH = ${LIBTAG_LIBRARY_PATH}")
+
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
+
+# configure json11
+
+set(CURRENT_EXTERNAL_PROJECT json11)
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ...")
+
+ExternalProject_Add(json11
+    PREFIX libjson11
+	GIT_REPOSITORY https://github.com/dropbox/json11.git
+    GIT_TAG v1.0.0
+    STEP_TARGETS build
+    EXCLUDE_FROM_ALL TRUE
+    CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF
+)
+
+ExternalProject_Get_Property(json11 SOURCE_DIR)
+ExternalProject_Get_Property(json11 BINARY_DIR)
+
+set(LIBJSON11_INCLUDE_PATH ${SOURCE_DIR})
+set(LIBJSON11_LIBRARY_PATH ${BINARY_DIR}/${CMAKE_BUILD_TYPE}/libjson11.a)
+
+message(STATUS "LIBJSON11_INCLUDE_PATH = ${LIBJSON11_INCLUDE_PATH}")
+message(STATUS "LIBJSON11_LIBRARY_PATH = ${LIBJSON11_LIBRARY_PATH}")
+
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
+
+# configure wxwidgets
+
+set(CURRENT_EXTERNAL_PROJECT wxwidgets)
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ...")
+
+ExternalProject_Add(wxwidgets
+    PREFIX libwxwidgets
+	GIT_REPOSITORY https://github.com/wxWidgets/wxWidgets.git
+    GIT_TAG v3.1.2
+    STEP_TARGETS build
+    EXCLUDE_FROM_ALL TRUE
+    CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF -DwxBUILD_SAMPLES=OFF -DwxBUILD_TESTS=OFF -DwxBUILD_DEMOS=OFF -DwxBUILD_INSTALL=OFF -DwxUSE_STL=ON -DwxUSE_STD_CONTAINERS=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 -DwxUSE_SOUND=OFF
+)
+
+ExternalProject_Get_Property(wxwidgets SOURCE_DIR)
+ExternalProject_Get_Property(wxwidgets BINARY_DIR)
+
+set(LIBWXWIDGETS_INCLUDE_PATH
+    ${BINARY_DIR}/lib/wx/include/osx_cocoa-unicode-static-3.1
+    ${SOURCE_DIR}/include
+)
+
+if(CMAKE_BUILD_TYPE MATCHES Debug)
+    set(LIBWXWIDGETS_LIBRARY_PATH
+        ${BINARY_DIR}/lib/${CMAKE_BUILD_TYPE}/libwx_baseud-3.1.a
+        ${BINARY_DIR}/lib/${CMAKE_BUILD_TYPE}/libwx_osx_cocoaud_core-3.1.a
+        ${BINARY_DIR}/lib/${CMAKE_BUILD_TYPE}/libwxpng-3.1.a
+    )
+else()
+    set(LIBWXWIDGETS_LIBRARY_PATH
+        ${BINARY_DIR}/lib/${CMAKE_BUILD_TYPE}/libwx_baseu-3.1.a
+        ${BINARY_DIR}/lib/${CMAKE_BUILD_TYPE}/libwx_osx_cocoau_core-3.1.a
+        ${BINARY_DIR}/lib/${CMAKE_BUILD_TYPE}/libwxpng-3.1.a
+    )
+endif()
+
+message(STATUS "LIBWXWIDGETS_INCLUDE_PATH = ${LIBWXWIDGETS_INCLUDE_PATH}")
+message(STATUS "LIBWXWIDGETS_LIBRARY_PATH = ${LIBWXWIDGETS_LIBRARY_PATH}")
+
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
