8b4dc24834e47b92b89457a6b011bfaf9b14eca7
Heiko Panjas <heiko@panjas.com>
2019-06-30 20:07:20 +0200

    fix linux build

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ################################################################################
 #
@@ -26,35 +26,197 @@
 #
 ################################################################################
 
-BUILD_DIRECTORY="build"
-ERROR_LEVEL=0
-if [ -x "$(command -v cmake)" ]; then
-    if [ -d "$BUILD_DIRECTORY" ]; then
-        if [ -L "$BUILD_DIRECTORY" ]; then
-            rm -f "$BUILD_DIRECTORY"
-        else
-            rm -rf "$BUILD_DIRECTORY"
-        fi
+CompareVersions () {
+  if [[ $1 == $2 ]]
+  then
+    return 0
+  fi
+  local IFS=.
+  local i ver1=($1) ver2=($2)
+  # fill empty fields in ver1 with zeros
+  for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+  do
+    ver1[i]=0
+  done
+  for ((i=0; i<${#ver1[@]}; i++))
+  do
+    if [[ -z ${ver2[i]} ]]
+      then
+        # fill empty fields in ver2 with zeros
+        ver2[i]=0
     fi
-    mkdir "$BUILD_DIRECTORY" && pushd "$BUILD_DIRECTORY"
-    echo 'Configuring projects...'
-    cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Debug ../
-    echo 'Done.'
-    if [ $? -eq 0 ]; then
-        echo 'Building projects...'
-        cmake --build . --target reaper_ultraschall --config Debug
-        echo 'Done.'
-        if [ $? -ne 0 ]; then
-            ERROR_LEVEL=$?
-            echo 'The cmake build step failed.' >&2
-        fi
-    else
-        ERROR_LEVEL=$?
-        echo 'Error: The cmake configure step failed.' >&2
+    if ((10#${ver1[i]} > 10#${ver2[i]}))
+    then
+      return 1
     fi
-    popd
-    exit $ERROR_LEVEL
-else
-    echo 'Error: cmake not found.' >&2
-    exit 1
+    if ((10#${ver1[i]} < 10#${ver2[i]}))
+    then
+      return 2
+    fi
+  done
+  return 0
+}
+
+TOOLS_DIRECTORY=`pwd`/_tools
+
+CMAKE_INSTALL_DIRECTORY=$TOOLS_DIRECTORY/cmake
+CMAKE_SOURCE_DIRECTORY=$CMAKE_INSTALL_DIRECTORY/src
+CMAKE_INSTALL_REQUIRED=1
+CMAKE_REQUIRED_VERSION="3.12.0"
+
+echo "Looking for CMake $CMAKE_REQUIRED_VERSION. Checking system install..."
+if [ $CMAKE_INSTALL_REQUIRED -ne 0 ]; then
+  CMAKE_INSTALL_PATH=cmake
+  if [ -x "$(command -v $CMAKE_INSTALL_PATH)" ]; then
+    CMAKE_CURRENT_VERSION=`$CMAKE_INSTALL_PATH --version | sort -V | tail -n 1 | awk -v n=3 '{print $n}'`
+    echo "Found CMake version $CMAKE_CURRENT_VERSION."
+    CompareVersions $CMAKE_CURRENT_VERSION $CMAKE_REQUIRED_VERSION
+    if [ ! $? -eq 2 ]; then
+      CMAKE_INSTALL_REQUIRED=0
+    fi
+  fi
 fi
+
+if [ $CMAKE_INSTALL_REQUIRED -ne 0 ]; then
+  echo "CMake $CMAKE_REQUIRED_VERSION does not seem to be installed on this system. Checking local install..."
+  CMAKE_INSTALL_PATH=$CMAKE_INSTALL_DIRECTORY/bin/cmake
+  if [ -x "$(command -v $CMAKE_INSTALL_PATH)" ]; then
+    CMAKE_CURRENT_VERSION=`$CMAKE_INSTALL_PATH --version | sort -V | tail -n 1 | awk -v n=3 '{print $n}'`
+    echo "Found CMake version $CMAKE_CURRENT_VERSION."
+    CompareVersions $CMAKE_CURRENT_VERSION $CMAKE_REQUIRED_VERSION
+    if [ ! $? -eq 2 ]; then
+      CMAKE_INSTALL_REQUIRED=0
+      export PATH=$CMAKE_INSTALL_DIRECTORY/bin:$PATH
+    fi
+  fi
+fi
+
+if [ $CMAKE_INSTALL_REQUIRED -ne 0 ]; then
+  echo "Installing CMake $CMAKE_REQUIRED_VERSION to $CMAKE_INSTALL_DIRECTORY..."
+
+  echo "Removing previous installations if there are any..."
+  if [ -d $CMAKE_SOURCE_DIRECTORY ]; then
+    rm -rf $CMAKE_SOURCE_DIRECTORY
+  fi
+  if [ -d $CMAKE_INSTALL_DIRECTORY ]; then
+    rm -rf $CMAKE_INSTALL_DIRECTORY
+  fi
+  echo "Done."
+
+  CMAKE_INSTALL_SUCCEEDED=1
+  if [ $CMAKE_INSTALL_SUCCEEDED -ne 0 ]; then
+    echo "Downloading CMake $CMAKE_REQUIRED_VERSION..."
+    git clone --recurse-submodules https://github.com/Kitware/CMake.git $CMAKE_SOURCE_DIRECTORY
+    if [ $? -eq 0 ]; then
+      echo "Done."
+    else
+      echo "Failed to download CMake."
+      CMAKE_INSTALL_SUCCEEDED=0
+    fi
+  fi
+
+  pushd $CMAKE_SOURCE_DIRECTORY > /dev/null
+
+  if [ $CMAKE_INSTALL_SUCCEEDED -ne 0 ]; then
+    echo "Checking out version $CMAKE_REQUIRED_VERSION..."
+    git fetch --all --tags --prune
+    git checkout tags/v3.12.0 -b v3.12.0 --recurse-submodules --no-track
+    if [ $? -eq 0 ]; then
+      echo "Done."
+    else
+      echo "Failed to checkout required tag."
+      CMAKE_INSTALL_SUCCEEDED=0
+    fi
+  fi
+
+  if [ $CMAKE_INSTALL_SUCCEEDED -ne 0 ]; then
+    ./configure --prefix=$CMAKE_INSTALL_DIRECTORY
+    if [ $? -eq 0 ]; then
+      echo "Done."
+    else
+      echo "Failed to configure CMake build."
+      CMAKE_INSTALL_SUCCEEDED=0
+    fi
+  fi
+
+  if [ $CMAKE_INSTALL_SUCCEEDED -ne 0 ]; then
+    make -j 6
+    if [ $? -eq 0 ]; then
+      echo "Done."
+    else
+      echo "Failed to build CMake."
+      CMAKE_INSTALL_SUCCEEDED=0
+    fi
+  fi
+
+  if [ $CMAKE_INSTALL_SUCCEEDED -ne 0 ]; then
+    make install
+    if [ $? -eq 0 ]; then
+      echo "Done."
+    else
+      echo "Failed to install CMake."
+      CMAKE_INSTALL_SUCCEEDED=0
+    fi
+  fi
+
+  if [ $CMAKE_INSTALL_SUCCEEDED -ne 0 ]; then
+    export PATH=$CMAKE_INSTALL_DIRECTORY/bin:$PATH
+    CMAKE_CURRENT_VERSION=`cmake --version | sort -V | tail -n 1 | awk -v n=3 '{print $n}'`
+    echo "CMAKE_CURRENT_VERSION  = $CMAKE_CURRENT_VERSION"
+    echo "CMAKE_REQUIRED_VERSION = $CMAKE_REQUIRED_VERSION"
+    CompareVersions $CMAKE_CURRENT_VERSION $CMAKE_REQUIRED_VERSION
+    if [ $? -eq 2 ]; then
+      CMAKE_INSTALL_SUCCEEDED=0
+    fi
+  fi
+
+  if [ $CMAKE_INSTALL_SUCCEEDED -eq 0 ]; then
+    echo "Failed to install CMake. Let's try anyway and hope for the best..."
+  fi
+
+  popd > /dev/null
+  echo "Done."
+fi
+
+CMAKE_GENERATOR="<unknown>"
+HOST_SYSTEM_TYPE=`uname`
+if [ "$HOST_SYSTEM_TYPE" = "Linux" ]; then
+  CMAKE_GENERATOR="Unix Makefiles"
+elif [ "$HOST_SYSTEM_TYPE" = "Darwin" ]; then
+  CMAKE_GENERATOR="Xcode"
+else
+  echo "Failed to detect the host system type. Only \"Linux\" or \"Darwin\" are supported. The current host system type is \"$HOST_SYSTEM_TYPE\""
+  exit -1
+fi
+
+BUILD_DIRECTORY=`pwd`/_build
+
+if [ ! -d $BUILD_DIRECTORY ]; then
+  mkdir $BUILD_DIRECTORY
+fi
+
+echo "Entering build directory..."
+pushd $BUILD_DIRECTORY > /dev/null
+echo "Done."
+
+echo "Configuring projects using $CMAKE_GENERATOR..."
+cmake -G"$CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=Debug ../
+if [ $? -ne 0 ]; then
+  echo "Failed to configure projects."
+  exit -1
+fi
+echo "Done."
+
+echo "Building projects using $CMAKE_GENERATOR..."
+cmake --build . --target reaper_ultraschall --config Debug -- -j 6
+if [ $? -ne 0 ]; then
+  echo "Failed to build projects."
+  exit -1
+fi
+echo "Done."
+
+echo "Leaving build directory..."
+popd > /dev/null
+echo "Done."
+
+exit 0
--- cmake/ubuntu.cmake
+++ cmake/ubuntu.cmake
@@ -4,23 +4,33 @@ set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG "$ENV{HOME}/.config/REAPER/UserPlugins"
 include(ExternalProject)
 
 # configure zlib
-
 set(CURRENT_EXTERNAL_PROJECT zlib)
 message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ...")
 
-find_package(ZLIB)
-if(ZLIB_FOUND)
-	message(STATUS "Found zlib version " ${ZLIB_VERSION_STRING})
+ExternalProject_Add(zlib
+  PREFIX libz
+  GIT_REPOSITORY https://github.com/madler/zlib.git
+  GIT_TAG v1.2.11
+  STEP_TARGETS build
+  EXCLUDE_FROM_ALL TRUE
+  CMAKE_ARGS -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF
+)
 
-	find_package(PkgConfig)
-	pkg_check_modules(UNZIP minizip)
-        find_library(minizip_LIBRARY minizip)
-	set(LIBZ_INCLUDE_PATH ${ZLIB_INCLUDE_DIRS} ${UNZIP_INCLUDEDIR})
-	set(LIBZ_LIBRARY_PATH ${ZLIB_LIBRARIES} ${minizip_LIBRARY})
-endif()
+ExternalProject_Get_Property(zlib SOURCE_DIR)
+ExternalProject_Get_Property(zlib BINARY_DIR)
+
+set(LIBZ_INCLUDE_PATH
+  ${BINARY_DIR}
+  ${SOURCE_DIR}
+  ${SOURCE_DIR}/contrib/minizip
+)
+
+set(LIBZ_LIBRARY_PATH ${BINARY_DIR}/libz.a)
+set(LIBZ_SOURCE_PATH ${SOURCE_DIR})
 
 message(STATUS "LIBZ_INCLUDE_PATH = ${LIBZ_INCLUDE_PATH}")
 message(STATUS "LIBZ_LIBRARY_PATH = ${LIBZ_LIBRARY_PATH}")
+message(STATUS "LIBZ_SOURCE_PATH  = ${LIBZ_SOURCE_PATH}")
 
 message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
 
@@ -29,15 +39,49 @@ message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
 set(CURRENT_EXTERNAL_PROJECT curl)
 message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ...")
 
-find_package(CURL)
-if(CURL_FOUND)
-	message(STATUS "Found curl version " ${CURL_VERSION_STRING})
-	set(LIBCURL_INCLUDE_PATH ${CURL_INCLUDE_DIRS})
-	set(LIBCURL_LIBRARY_PATH ${CURL_LIBRARIES})
-endif()
+ExternalProject_Add(curl
+  PREFIX libcurl
+  GIT_REPOSITORY https://github.com/curl/curl.git
+  GIT_TAG curl-7_63_0
+  STEP_TARGETS build
+  EXCLUDE_FROM_ALL TRUE
+  CMAKE_ARGS -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_CURL_EXE=OFF -DBUILD_SHARED_LIBS=OFF -DHTTP_ONLY=ON -DENABLE_IPV6=ON -DCMAKE_USE_OPENSSL=ON -DCMAKE_USE_LIBSSH2=OFF -DCURL_CA_PATH=none -DBUILD_TESTING=OFF
+)
+
+ExternalProject_Get_Property(curl SOURCE_DIR)
+ExternalProject_Get_Property(curl BINARY_DIR)
+
+set(LIBCURL_INCLUDE_PATH ${SOURCE_DIR}/include)
+set(LIBCURL_LIBRARY_PATH ${BINARY_DIR}/lib/libcurl.a)
 
 message(STATUS "LIBCURL_INCLUDE_PATH = ${LIBCURL_INCLUDE_PATH}")
 message(STATUS "LIBCURL_LIBRARY_PATH = ${LIBCURL_LIBRARY_PATH}")
+
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
+
+# configure mp4v2
+
+set(CURRENT_EXTERNAL_PROJECT mp4v2)
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ...")
+
+include(ExternalProject)
+ExternalProject_Add(mp4v2
+  PREFIX libmp4v2
+  GIT_REPOSITORY https://github.com/Ultraschall/mp4v2.git
+  GIT_TAG v2.1.0
+  STEP_TARGETS build
+  EXCLUDE_FROM_ALL TRUE
+  CMAKE_ARGS -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF -DMP4V2_EXPORTS=0 -DMP4V2_NO_STDINT_DEFS=1
+)
+
+ExternalProject_Get_Property(mp4v2 SOURCE_DIR)
+ExternalProject_Get_Property(mp4v2 BINARY_DIR)
+
+set(LIBMP4V2_INCLUDE_PATH ${SOURCE_DIR}/include)
+set(LIBMP4V2_LIBRARY_PATH ${BINARY_DIR}/libmp4v2.a)
+
+message(STATUS "LIBMP4V2_INCLUDE_PATH = ${LIBMP4V2_INCLUDE_PATH}")
+message(STATUS "LIBMP4V2_LIBRARY_PATH = ${LIBMP4V2_LIBRARY_PATH}")
 
 message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
 
@@ -48,24 +92,24 @@ message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ..
 
 include(ExternalProject)
 ExternalProject_Add(taglib
-    PREFIX libtag
-	GIT_REPOSITORY https://github.com/taglib/taglib.git
-	GIT_TAG v1.11.1
-    STEP_TARGETS build
-    EXCLUDE_FROM_ALL TRUE
-	CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_BINDINGS=OFF
+  PREFIX libtag
+  GIT_REPOSITORY https://github.com/taglib/taglib.git
+  GIT_TAG v1.11.1
+  STEP_TARGETS build
+  EXCLUDE_FROM_ALL TRUE
+  CMAKE_ARGS -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_BINDINGS=OFF
 )
 
 ExternalProject_Get_Property(taglib SOURCE_DIR)
 ExternalProject_Get_Property(taglib BINARY_DIR)
 
 set(LIBTAG_INCLUDE_PATH
-    ${BINARY_DIR} # taglib_config.h
-    ${SOURCE_DIR}/taglib
-    ${SOURCE_DIR}/taglib/toolkit
-    ${SOURCE_DIR}/taglib/mpeg
-    ${SOURCE_DIR}/taglib/mpeg/id3v2
-    ${SOURCE_DIR}/taglib/mpeg/id3v2/frames
+  ${BINARY_DIR} # taglib_config.h
+  ${SOURCE_DIR}/taglib
+  ${SOURCE_DIR}/taglib/toolkit
+  ${SOURCE_DIR}/taglib/mpeg
+  ${SOURCE_DIR}/taglib/mpeg/id3v2
+  ${SOURCE_DIR}/taglib/mpeg/id3v2/frames
 )
 
 set(LIBTAG_LIBRARY_PATH   ${BINARY_DIR}/taglib/libtag.a)
@@ -75,50 +119,18 @@ message(STATUS "LIBTAG_LIBRARY_PATH = ${LIBTAG_LIBRARY_PATH}")
 
 message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
 
-# configure mp4v2
-
-set(CURRENT_EXTERNAL_PROJECT mp4v2)
-message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring...")
-
-find_package(PkgConfig)
-find_library(mp4v2_LIBRARY mp4v2)
-set(LIBMP4V2_INCLUDE_PATH /usr/include)
-set(LIBMP4V2_LIBRARY_PATH ${mp4v2_LIBRARY})
-
-message(STATUS "LIBMP4V2_INCLUDE_PATH = ${LIBMP4V2_INCLUDE_PATH}")
-message(STATUS "LIBMP4V2_LIBRARY_PATH = ${LIBMP4V2_LIBRARY_PATH}")
-
-message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
-
-# configure expat
-
-set(CURRENT_EXTERNAL_PROJECT expat)
-message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring...")
-
-find_package(EXPAT)
-if(EXPAT_FOUND)
-	message(STATUS "Found expat version " ${EXPAT_VERSION_STRING})
-	set(LIBEXPAT_INCLUDE_PATH ${EXPAT_INCLUDE_DIRS})
-	set(LIBEXPAT_LIBRARY_PATH ${EXPAT_LIBRARIES})
-endif()
-
-message(STATUS "LIBEXPAT_INCLUDE_PATH = ${LIBEXPAT_INCLUDE_PATH}")
-message(STATUS "LIBEXPAT_LIBRARY_PATH = ${LIBEXPAT_LIBRARY_PATH}")
-
-message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
-
 # configure json11
 
 set(CURRENT_EXTERNAL_PROJECT json11)
 message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ...")
 
 ExternalProject_Add(json11
-    PREFIX libjson11
-	GIT_REPOSITORY https://github.com/dropbox/json11.git
-    GIT_TAG v1.0.0
-    STEP_TARGETS build
-    EXCLUDE_FROM_ALL TRUE
-    CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF
+  PREFIX libjson11
+  GIT_REPOSITORY https://github.com/dropbox/json11.git
+  GIT_TAG v1.0.0
+  STEP_TARGETS build
+  EXCLUDE_FROM_ALL TRUE
+  CMAKE_ARGS -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF
 )
 
 ExternalProject_Get_Property(json11 SOURCE_DIR)
@@ -132,23 +144,51 @@ message(STATUS "LIBJSON11_LIBRARY_PATH = ${LIBJSON11_LIBRARY_PATH}")
 
 message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
 
+# configure expat
+
+set(CURRENT_EXTERNAL_PROJECT expat)
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ...")
+ExternalProject_Add(expat
+  PREFIX libexpat
+  SOURCE_SUBDIR expat
+  GIT_REPOSITORY https://github.com/libexpat/libexpat.git
+  GIT_TAG R_2_2_6
+  STEP_TARGETS build
+  EXCLUDE_FROM_ALL TRUE
+  CMAKE_ARGS -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=OFF -DBUILD_doc=OFF -DINSTALL=OFF -DXML_DTD=OFF -DXML_NS=ON
+)
+ExternalProject_Get_Property(expat SOURCE_DIR)
+ExternalProject_Get_Property(expat BINARY_DIR)
+
+set(LIBEXPAT_INCLUDE_PATH ${SOURCE_DIR})
+set(LIBEXPAT_LIBRARY_PATH ${BINARY_DIR}/libexpat.a)
+
+message(STATUS "LIBEXPAT_INCLUDE_PATH = ${LIBEXPAT_INCLUDE_PATH}")
+message(STATUS "LIBEXPAT_LIBRARY_PATH = ${LIBEXPAT_LIBRARY_PATH}")
+
+message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
+
 # configure wdl
 
 set(CURRENT_EXTERNAL_PROJECT wdl)
 message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Configuring ...")
 
 ExternalProject_Add(wdl
-    PREFIX libwdl
-    SOURCE_SUBDIR wdl
-	GIT_REPOSITORY https://github.com/justinfrankel/WDL.git
-    STEP_TARGETS build
-    EXCLUDE_FROM_ALL TRUE
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
+  PREFIX libwdl
+  SOURCE_SUBDIR wdl
+  GIT_REPOSITORY https://github.com/justinfrankel/WDL.git
+  STEP_TARGETS build
+  EXCLUDE_FROM_ALL TRUE
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
 )
 
 ExternalProject_Get_Property(wdl SOURCE_DIR)
+# ExternalProject_Get_Property(wdl BINARY_DIR)
 set(LIBWDL_INCLUDE_PATH ${SOURCE_DIR})
+# set(LIBWDL_LIBRARY_PATH ${BINARY_DIR}/${CMAKE_BUILD_TYPE}/libwdl.a)
+
 message(STATUS "LIBWDL_INCLUDE_PATH = ${LIBWDL_INCLUDE_PATH}")
+message(STATUS "LIBWDL_LIBRARY_PATH = ${LIBWDL_LIBRARY_PATH}")
 
 message(STATUS "${CURRENT_EXTERNAL_PROJECT}<${CMAKE_BUILD_TYPE}>: Done.")
